03d2c764ca75b570d3594a0f10f184e6ec01b7a8
Joyce Chun <wooyangee@gmail.com>
2018-04-28 22:27:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt () {
 
 function pressIt() {
   $('input').on('keydown', function(n) {
-    if (n.which === "G") 
+    if (n.which === "G") {
       alert("You put G!");
     }
   });
