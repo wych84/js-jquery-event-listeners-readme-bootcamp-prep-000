8a722dd20d2d14d56c6a4d6968b88d4f49aa2791
Joyce Chun <wooyangee@gmail.com>
2018-04-28 22:30:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,8 +20,8 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function() {
-    
-  })
+    alert("Your form is going to be submitted now.");
+  });
 }
 $(document).ready(function(){
 
