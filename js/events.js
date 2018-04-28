9dd41bbf74908308009d55f126d92fd629d17d71
Joyce Chun <wooyangee@gmail.com>
2018-04-28 22:30:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
-//define functions here
+function getIt () {
+  $('p').on('click', function () {
+    alert('Hey!');
+  });
+}
+
+function frameIt () {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(n) {
+    if (n.which === 71) {
+      alert("You put G!");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
